901cacb27bc1dd6edc4098e05e63c54d29134ef4
Dena Neuman <denaweiss5@gmail.com>
2020-10-16 15:06:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,9 @@
-#require 'pry'
+
 
 def nyc_pigeon_organizer(data)
-final_results= date.each_with_object({}) do |(key ,value), final_array|
-  #binding.pry
-  value.each do |inner_key,names|
+final_results = date.each_with_object({}) do |(key ,value), final_array|
+  value.each do |inner_key, names|
     names.each do|name|
-      #binding.pry
       if !final_array[name]
         final_array[name]= {}
       end
@@ -13,8 +11,7 @@ final_results= date.each_with_object({}) do |(key ,value), final_array|
         !final_array[name][key]= []
       end
       final_array[name][key].push(inner_key.to_s)
+      end
+    end
   end
-end
-end
-#binding.pry
 end
